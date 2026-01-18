6801d88ebfb48b9e55d499d85e58fe3b496387fa
sivanagi reddy akkili <sivanagireddyakkili@gmail.com>
2026-01-18 22:34:00 +0530

    updated the passing functions code

--- src/PassingFunctions.java
+++ src/PassingFunctions.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class PassingFunctions {
     public static void main(String[] args) {
+        int[] nums={3,4,2,1};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+
+        System.out.println(2);
+    	//arrays are muatable in java
+        // strings are immuatable in java
+
+    }
+    static void change (int[] arr){
+        arr[0]=99;
+
 
     }
 }
