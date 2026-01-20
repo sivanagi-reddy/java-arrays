751e1ecbbc10125ef8cd7c871e1deb6d2e1118a5
sivanagi reddy akkili <sivanagireddyakkili@gmail.com>
2026-01-20 23:59:27 +0530

    updated swap.java file

--- src/Swap.java
+++ src/Swap.java
@@ -5,15 +5,32 @@ public class Swap {
         int[] list = {1,2,3,4,5};
         swap(list,0,3);
 
+        reverse(list);
+
+
         System.out.println(Arrays.toString(list));
 
 
     }
+    static void reverse(int[]list){
+        int start =0;
+        int end =list.length-1;
+        while(start<end){
+            //swap
+            swap(list,start,end);
+            start++;
+            end--;
 
-    static void swap(int[] list, int i, int j) {
-        int temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
+
+        }
+
+    }
+
+
+    static void swap(int[] list, int index1, int index2) {
+        int temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
     }
 
 }
